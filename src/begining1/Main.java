51216846ee87842c88ef87b90dec61a3bd2250c8
Egor0603 <hardgames123@mail.ru>
2022-02-26 15:06:52 +0300

    first commit

---
+++ src/begining1/Main.java
@@ -0,0 +1,11 @@
+package begining1;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("sum = " + c);
+
+    }
+}
